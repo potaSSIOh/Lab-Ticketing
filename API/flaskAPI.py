bae7285724af6e424509326c509a4aa19ee2a525
Mattia Dorella <mattiadorella70@gmail.com>
2025-04-06 19:17:09 +0200

    Added the function flask_cors

--- API/flaskAPI.py
+++ API/flaskAPI.py
@@ -1,10 +1,11 @@
 #REMEMBER TO IMPORT FLASK AND PYMYSQL IN THE MACHINE
 from flask import Flask,jsonify,request,make_response 
-import pymysql 
+import pymysql
+from flask_cors import CORS 
 db = pymysql.connect(host="127.0.0.1", user="root", password="", database="labticketing",autocommit=True, cursorclass=pymysql.cursors.DictCursor)
 app = Flask(__name__)
-
-# GET method
+CORS(app)
+# GET methods
 
 @app.route('/',methods=['GET'])
 def hello():
@@ -34,31 +35,41 @@ def utenti():
     results = cursor.fetchall() 
     return jsonify(results),200
 
-@app.route('/fissi/<int:Aula>', methods=['GET']) 
-def fissi(Aula): 
+@app.route('/fissi', methods=['GET'])
+@app.route('/fissi/<int:Aula>', methods=['GET'])
+def fissi(Aula=None):
     cursor = db.cursor()
-    sql = "SELECT * FROM fissi WHERE Aula = %s"
-    cursor.execute(sql, (Aula,))
-    result = cursor.fetchone()
+    if Aula is not None:
+        sql = "SELECT * FROM fissi WHERE Aula = %s"
+        cursor.execute(sql, (Aula,))
+    else:
+        sql = "SELECT * FROM fissi"
+        cursor.execute(sql)
+        
+    results = cursor.fetchall()
 
-    if result is None:
+    if not results:
         return jsonify({"Error": "risorsa non trovata"}), 404
     else:
-        return jsonify(result), 200
+        return jsonify(results), 200
 
-
-
+@app.route('/portatili', methods=['GET'])
 @app.route('/portatili/<string:codBox>',methods=['GET']) 
-def portatili():
-    cursor = db.cursor() 
-    codBox = "%s"
-    sql = "SELECT * FROM portatili where codBox=%s" 
-    cursor.execute(sql, (codBox)) 
-    results = cursor.fetchone() 
-    if results==None:
-        return jsonify({"Error":"risorsa non trovata"}),404
+def portatili(codBox=None):
+    cursor = db.cursor()
+    if codBox is not None:
+        sql = "SELECT * FROM portatili WHERE codBox = %s"
+        cursor.execute(sql, (codBox,))
     else:
-        return jsonify(results),200
+        sql = "SELECT * FROM portatili"
+        cursor.execute(sql)
+        
+    results = cursor.fetchall()
+
+    if not results:
+        return jsonify({"Error": "risorsa non trovata"}), 404
+    else:
+        return jsonify(results), 200
 
 @app.route('/ticket',methods=['GET']) 
 def ticket():
@@ -68,6 +79,22 @@ def ticket():
     results = cursor.fetchall() 
     return jsonify(results),200
 
+# POST methods
 
+#Content-Type:application/json 
+@app.route('/utenti', methods=['POST'])
+def addUtenti(name_mail, password, autorizzato):
+    data = request.json 
+    cursor = db.cursor() 
+    sql = "insert into utenti (name_mail, password, autorizzato) values (%s, %s, %s)" 
+    cursor.execute(sql,(data["name_mail"]))
+    cursor.execute(sql,(data["password"]))
+    cursor.execute(sql,(data["autorizzato"]))  
+    if cursor.rowcount==0: 
+        res=make_response(jsonify({"Error":"risorsa non inserita"}),403) 
+    else: 
+        res=make_response(jsonify({"Status":"OK "}),201) 
+        res.headers.add("location","/utenti/"+str(cursor.lastrowid)) 
+    return res
 
 app.run()
