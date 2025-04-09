b4e1a2888965b3e570afb6e48244cf0b2b97f9fe
Marco Dai Prè <daipre.marco@gmail.com>
2025-04-09 06:12:40 +0200

    introduced the POST and the PATCH methods

--- API/flaskAPI.py
+++ API/flaskAPI.py
@@ -5,11 +5,9 @@ from flask_cors import CORS
 db = pymysql.connect(host="127.0.0.1", user="root", password="", database="labticketing",autocommit=True, cursorclass=pymysql.cursors.DictCursor)
 app = Flask(__name__)
 CORS(app)
-# GET methods
 
-@app.route('/',methods=['GET'])
-def hello():
-    return "hello"
+#----------------------------------------------------------------------------------------
+# GET methods
 
 @app.route('/aule',methods=['GET']) 
 def aule():
@@ -45,9 +43,7 @@ def fissi(Aula=None):
     else:
         sql = "SELECT * FROM fissi"
         cursor.execute(sql)
-        
     results = cursor.fetchall()
-
     if not results:
         return jsonify({"Error": "risorsa non trovata"}), 404
     else:
@@ -63,7 +59,6 @@ def portatili(codBox=None):
     else:
         sql = "SELECT * FROM portatili"
         cursor.execute(sql)
-        
     results = cursor.fetchall()
 
     if not results:
@@ -79,22 +74,197 @@ def ticket():
     results = cursor.fetchall() 
     return jsonify(results),200
 
+#----------------------------------------------------------------------------------------
 # POST methods
 
-#Content-Type:application/json 
+#metodo per aggiungere un utente
 @app.route('/utenti', methods=['POST'])
-def addUtenti(name_mail, password, autorizzato):
-    data = request.json 
+def addUtenti():
+    data = request.get_json()
+    try:
+        name_mail = data["name_mail"]
+        password = data["password"]
+        autorizzato = data["autorizzato"]
+    except KeyError as e:
+        return make_response(jsonify({"Error": f"Campo mancante: {str(e)}"}), 400)
     cursor = db.cursor() 
     sql = "insert into utenti (name_mail, password, autorizzato) values (%s, %s, %s)" 
-    cursor.execute(sql,(data["name_mail"]))
-    cursor.execute(sql,(data["password"]))
-    cursor.execute(sql,(data["autorizzato"]))  
+    cursor.execute(sql, (name_mail, password, autorizzato)) 
     if cursor.rowcount==0: 
         res=make_response(jsonify({"Error":"risorsa non inserita"}),403) 
     else: 
         res=make_response(jsonify({"Status":"OK "}),201) 
         res.headers.add("location","/utenti/"+str(cursor.lastrowid)) 
     return res
+
+#metodo per aggiungere un ticket
+@app.route('/ticket', methods=['POST'])
+def addTicket():
+    data = request.get_json()
+    try:
+        descrizione = data["descrizione"]
+        dataOra = data["dataOra"]
+        creatore = data["creatore"]
+        hostnameF = data["hostnameF"]
+        hostnameP = data["hostnameP"]
+        tecnico = data["tecnico"]
+        stato = data["stato"]
+    except KeyError as e:
+        return make_response(jsonify({"Error": f"Campo mancante: {str(e)}"}), 400)
+    cursor = db.cursor() 
+    sql = "insert into ticket (descrizione, dataOra, creatore, hostnameF, hostnameP, tecnico, stato) values (%s, %s, %s, %s, %s, %s, %s)" 
+    cursor.execute(sql, (descrizione, dataOra, creatore, hostnameF, hostnameP, tecnico, stato)) 
+    if cursor.rowcount==0: 
+        res=make_response(jsonify({"Error":"risorsa non inserita"}),403) 
+    else: 
+        res=make_response(jsonify({"Status":"OK "}),201) 
+        res.headers.add("location","/ticket/"+str(cursor.lastrowid)) 
+    return res
+
+#metodo per aggiungere un fisso
+@app.route('/fissi', methods=['POST'])
+def addFisso():
+    data = request.get_json()
+    try:
+        HostName = data["HostName"]
+        Aula = data["Aula"]
+    except KeyError as e:
+        return make_response(jsonify({"Error": f"Campo mancante: {str(e)}"}), 400)
+    cursor = db.cursor() 
+    sql = "insert into fissi (HostName, Aula) values (%s, %s)" 
+    cursor.execute(sql, (HostName, Aula)) 
+    if cursor.rowcount==0: 
+        res=make_response(jsonify({"Error":"risorsa non inserita"}),403) 
+    else: 
+        res=make_response(jsonify({"Status":"OK "}),201) 
+        res.headers.add("location","/fissi/"+str(cursor.lastrowid)) 
+    return res
+
+#metodo per aggiungere un'aula
+@app.route('/aule', methods=['POST'])
+def addAula():
+    data = request.get_json()
+    try:
+        nAula = data["nAula"]
+        Lab = data["Lab"]
+    except KeyError as e:
+        return make_response(jsonify({"Error": f"Campo mancante: {str(e)}"}), 400)
+    cursor = db.cursor() 
+    sql = "insert into aule (nAula, Lab) values (%s, %s)" 
+    cursor.execute(sql, (nAula, Lab)) 
+    if cursor.rowcount==0: 
+        res=make_response(jsonify({"Error":"risorsa non inserita"}),403) 
+    else: 
+        res=make_response(jsonify({"Status":"OK "}),201) 
+        res.headers.add("location","/aule/"+str(cursor.lastrowid)) 
+    return res
+
+#metodo per aggiungere un portatile
+@app.route('/portatili', methods=['POST'])
+def addPortatile():
+    data = request.get_json()
+    try:
+        HostName = data["HostName"]
+        codBox = data["codBox"]
+    except KeyError as e:
+        return make_response(jsonify({"Error": f"Campo mancante: {str(e)}"}), 400)
+    cursor = db.cursor() 
+    sql = "insert into portatili (HostName, codBox) values (%s, %s)" 
+    cursor.execute(sql, (HostName, codBox)) 
+    if cursor.rowcount==0: 
+        res=make_response(jsonify({"Error":"risorsa non inserita"}),403) 
+    else: 
+        res=make_response(jsonify({"Status":"OK "}),201) 
+        res.headers.add("location","/portatili/"+str(cursor.lastrowid)) 
+    return res
+
+#metodo per aggiungere un box
+@app.route('/box', methods=['POST'])
+def addBox():
+    data = request.get_json()
+    try:
+        codBox = data["codBox"]
+    except KeyError as e:
+        return make_response(jsonify({"Error": f"Campo mancante: {str(e)}"}), 400)
+    cursor = db.cursor() 
+    sql = "insert into box (codBox) values (%s)" 
+    cursor.execute(sql, (codBox)) 
+    if cursor.rowcount==0: 
+        res=make_response(jsonify({"Error":"risorsa non inserita"}),403) 
+    else: 
+        res=make_response(jsonify({"Status":"OK "}),201) 
+        res.headers.add("location","/box/"+str(cursor.lastrowid)) 
+    return res
+
+#----------------------------------------------------------------------------------------
+# PATCH method
+
+#metodo per modificare la password di un utente
+@app.route('/utenti/<int:id>', methods=['PATCH'])
+def update_password(id):
+    try:
+        data = request.get_json()
+    except KeyError:
+        return make_response(jsonify({"Error": "Campo 'descrizione' mancante"}), 400)
+    new_password = data["password"]
+    cursor = db.cursor()
+    sql = "UPDATE utenti SET password = %s WHERE id = %s"
+    cursor.execute(sql, (new_password, id))
+    db.commit()
+
+    if cursor.rowcount == 0:
+        return make_response(jsonify({"Error": "Utente non trovato o password non modificata"}), 404)
+
+    return make_response(jsonify({"Status": "Password aggiornata"}), 200)
+
+#metodo per modificare la descrizione di un ticket
+@app.route('/ticket/<int:id>', methods=['PATCH'])
+def update_ticket_description(id):
+    data = request.get_json()
+    try:
+        new_descrizione = data["descrizione"]
+    except KeyError:
+        return make_response(jsonify({"Error": "Campo 'descrizione' mancante"}), 400)
+    cursor = db.cursor()
+    sql = "UPDATE ticket SET descrizione = %s WHERE id = %s"
+    cursor.execute(sql, (new_descrizione, id))
+    db.commit()
+    if cursor.rowcount == 0:
+        return make_response(jsonify({"Error": "Ticket non trovato o descrizione non modificata"}), 404)
+    return make_response(jsonify({"Status": "Descrizione aggiornata"}), 200)
+
+#metodo per modificare il tecnico
+@app.route('/ticket/<int:id>/tecnico', methods=['PATCH'])
+def update_ticket_tecnico(id):
+    data = request.get_json()
+    try:
+        new_tecnico = data["tecnico"]
+    except KeyError:
+        return make_response(jsonify({"Error": "Campo 'tecnico' mancante"}), 400)
+    cursor = db.cursor()
+    sql = "UPDATE ticket SET tecnico = %s WHERE id = %s"
+    cursor.execute(sql, (new_tecnico, id))
+    db.commit()
+    if cursor.rowcount == 0:
+        return make_response(jsonify({"Error": "Ticket non trovato o tecnico non modificato"}), 404)
+    return make_response(jsonify({"Status": "Tecnico aggiornato"}), 200)
+
+#metodo per cambiare lo stato dei ticket
+@app.route('/ticket/<int:id>/stato', methods=['PATCH'])
+def update_ticket_stato(id):
+    data = request.get_json()
+    try:
+        new_stato = data["stato"]
+    except KeyError:
+        return make_response(jsonify({"Error": "Campo 'stato' mancante"}), 400)
+    cursor = db.cursor()
+    sql = "UPDATE ticket SET stato = %s WHERE id = %s"
+    cursor.execute(sql, (new_stato, id))
+    db.commit()
+    if cursor.rowcount == 0:
+        return make_response(jsonify({"Error": "Ticket non trovato o stato non modificato"}), 404)
+    return make_response(jsonify({"Status": "Stato aggiornato"}), 200)
+
+#----------------------------------------------------------------------------------------
 
 app.run()
