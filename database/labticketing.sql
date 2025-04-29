b54585b7873b9cba3dfa218f9e3c452ef3c4fa5d
Doremla <mattiadorella70@gmail.com>
2025-04-29 08:29:08 +0200

    Updated the Database

--- database/labticketing.sql
+++ database/labticketing.sql
@@ -28,11 +28,7 @@ CREATE TABLE IF NOT EXISTS `aule` (
   PRIMARY KEY (`nAula`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella labticketing.aule: ~3 rows (circa)
-REPLACE INTO `aule` (`nAula`, `Lab`) VALUES
-	(147, 1),
-	(247, 1),
-	(347, 1);
+-- L’esportazione dei dati non era selezionata.
 
 -- Dump della struttura di tabella labticketing.box
 DROP TABLE IF EXISTS `box`;
@@ -41,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `box` (
   PRIMARY KEY (`codBox`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella labticketing.box: ~0 rows (circa)
+-- L’esportazione dei dati non era selezionata.
 
 -- Dump della struttura di tabella labticketing.fissi
 DROP TABLE IF EXISTS `fissi`;
@@ -53,15 +49,7 @@ CREATE TABLE IF NOT EXISTS `fissi` (
   CONSTRAINT `fissi_ibfk_1` FOREIGN KEY (`Aula`) REFERENCES `aule` (`nAula`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella labticketing.fissi: ~7 rows (circa)
-REPLACE INTO `fissi` (`HostName`, `Aula`) VALUES
-	('W10STAT-7-157', 247),
-	('W10STAT-7-159', 247),
-	('W10STAT-7-160', 247),
-	('W10STAT-7-161', 247),
-	('W10STAT-7-162', 247),
-	('W10STAT-7-163', 247),
-	('W10STAT-7-164', 247);
+-- L’esportazione dei dati non era selezionata.
 
 -- Dump della struttura di tabella labticketing.portatili
 DROP TABLE IF EXISTS `portatili`;
@@ -73,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `portatili` (
   CONSTRAINT `portatili_ibfk_1` FOREIGN KEY (`codBox`) REFERENCES `box` (`codBox`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella labticketing.portatili: ~0 rows (circa)
+-- L’esportazione dei dati non era selezionata.
 
 -- Dump della struttura di tabella labticketing.ticket
 DROP TABLE IF EXISTS `ticket`;
@@ -98,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `ticket` (
   CONSTRAINT `check_stato` CHECK (`stato` in ('In lavorazione','Chiuso','Aperto'))
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella labticketing.ticket: ~0 rows (circa)
+-- L’esportazione dei dati non era selezionata.
 
 -- Dump della struttura di tabella labticketing.utenti
 DROP TABLE IF EXISTS `utenti`;
@@ -109,11 +97,9 @@ CREATE TABLE IF NOT EXISTS `utenti` (
   `autorizzato` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_mail` (`name_mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dump dei dati della tabella labticketing.utenti: ~1 rows (circa)
-REPLACE INTO `utenti` (`id`, `name_mail`, `password`, `autorizzato`) VALUES
-	(1, 'IlBoss', '8gz5CMb3', 1);
+-- L’esportazione dei dati non era selezionata.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
