006fa9085616869e380528829b930d3296ce8710
Marco Dai Prè <daipre.marco@gmail.com>
2025-05-13 19:32:42 +0200

    added ticketp_routes.py and modified app.py

--- API/app.py
+++ API/app.py
@@ -5,6 +5,7 @@ from routes.box_routes import box_routes
 from routes.fissi_routes import fissi_routes
 from routes.portatili_routes import portatili_routes
 from routes.ticketf_routes import ticketf_routes
+from routes.ticketp_routes import ticketp_routes
 from routes.utenti_routes import utenti_routes
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ app.register_blueprint(box_routes)
 app.register_blueprint(fissi_routes)
 app.register_blueprint(portatili_routes)
 app.register_blueprint(ticketf_routes)
+app.register_blueprint(ticketp_routes)
 app.register_blueprint(utenti_routes)
 
 if __name__ == '__main__':
